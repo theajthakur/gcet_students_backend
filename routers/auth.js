4c1e4e3679e11176b17d01a9f2474b4f051e456b
Vijay Singh <thakurvijayofficial@gmail.com>
2024-09-16 22:53:55 +0530

    Added Checklogin Route to validate client token

--- routers/auth.js
+++ routers/auth.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const { handleLogin } = require("../controllers/handleLogin");
-router.post("/login", handleLogin);
+require("dotenv").config();
 
+router.post("/login", handleLogin);
+router.get("/checklogin", (req, res) => {
+  const token = req.header("Authorization");
+  console.log(token);
+  if (!token)
+    return res.status(401).json({ status: "error", message: "unauthorised" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.json({ status: "error", message: err });
+    return res.json({
+      status: "success",
+      message: `${user.name} is verified!`,
+    });
+  });
+});
 module.exports = router;
