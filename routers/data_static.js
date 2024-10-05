7b60d737c50491c4518b85ff60da63aeb3022311
Vijay Singh <thakurvijayofficial@gmail.com>
2024-10-06 03:16:48 +0530

    Fetch User by ID

--- routers/data_static.js
+++ routers/data_static.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { fetch_student } = require("../controllers/handleStudent");
+const {
+  fetch_student,
+  student_profile,
+} = require("../controllers/handleStudent");
 router.get("/", fetch_student);
+router.get("/:id", student_profile);
 
 module.exports = router;
