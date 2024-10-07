2f196a111f9d81d5d726dd22621dbf3bd1a1fa0e
Vijay Singh <thakurvijayofficial@gmail.com>
2024-10-08 00:26:13 +0530

    Organised Auth Router

--- routers/auth.js
+++ routers/auth.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const { handleLogin } = require("../controllers/handleLogin");
+const { handleLogin, checkLogin } = require("../controllers/handleLogin");
 require("dotenv").config();
 
 router.post("/login", handleLogin);
-router.get("/checklogin", (req, res) => {
-  const token = req.header("Authorization");
-  if (!token)
-    return res.status(401).json({ status: "error", message: "unauthorised" });
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.json({ status: "error", message: err });
-    return res.json({
-      status: "success",
-      message: `${user.name} is verified!`,
-    });
-  });
-});
+router.get("/checklogin", checkLogin);
 module.exports = router;
