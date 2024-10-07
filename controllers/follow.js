4c590ba5a8755b8a2758f3882a33f1790f3e1383
Vijay Singh <thakurvijayofficial@gmail.com>
2024-10-08 00:53:14 +0530

    Improved Follow Request Controller

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,4 +1,5 @@
 const Follow = require("../models/follow");
+const Student = require("../models/Students");
 
 async function handleRequest(req, res) {
   const user = req.user;
@@ -10,14 +11,35 @@ async function handleRequest(req, res) {
   }
 
   try {
+    const secondUser = await Student.findOne({ where: { sr_no: following } });
+    if (!secondUser) {
+      return res.status(404).json({ success: false, error: "No User found" });
+    }
+
+    const existingRequest = await Follow.findOne({
+      where: { followerId: follower, followingId: following },
+    });
+
+    if (existingRequest) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Follow request already exists" });
+    }
+
     await Follow.create({ followerId: follower, followingId: following });
 
     return res.status(200).json({
       success: true,
-      message: "Follow request sent successfully!",
+      message: `Follow request to ${secondUser.name} has been sent successfully!`,
     });
   } catch (error) {
-    console.error("Error creating follow request:", error);
+    console.error(
+      "Error creating follow request for user:",
+      follower,
+      "to:",
+      following,
+      error
+    );
     return res.status(500).json({
       success: false,
       error: "Something went wrong. Please try again later.",
