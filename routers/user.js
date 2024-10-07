eba406727c210a4597f78d049eef43c05bffc0c5
Vijay Singh <thakurvijayofficial@gmail.com>
2024-10-08 00:23:54 +0530

    Organise User Router

--- routers/user.js
+++ routers/user.js
@@ -1,31 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Students = require("../models/Students");
+const { showProfile, updateProfile } = require("../controllers/handleUser");
 require("dotenv").config();
 
-router.post("/profile", async (req, res) => {
-  const { mobile, email } = req.body;
-  if (!mobile || !email) return res.end("Invalid Request!");
-  const user = req.user;
-  try {
-    await Students.update(
-      { email: email, mobile: mobile },
-      { where: { sr_no: user.sr_no, adm_no: user.adm_no } }
-    );
-    res.json({ status: "updated!" });
-  } catch (error) {
-    res.json({ error: error });
-  }
-});
-
-router.get("/profile", async (req, res) => {
-  try {
-    const user = req.user;
-    const profile = await Students.findOne({ where: { sr_no: user.sr_no } });
-    res.json(profile);
-  } catch (error) {
-    res.json({ error: error });
-  }
-});
+// Router for Logged in User at /user
+router.post("/profile", updateProfile);
+router.get("/profile", showProfile);
 
 module.exports = router;
