943d293fb787bbf107e284cdacb0ff2bd5e12ada
Vijay Singh <thakurvijayofficial@gmail.com>
2024-10-10 23:11:02 +0530

    Route to fetch Requests

--- routers/follow.js
+++ routers/follow.js
@@ -4,11 +4,14 @@ const {
   handleAccept,
   handleRemove,
   handleRequest,
+  listRequests,
 } = require("../controllers/follow");
 require("dotenv").config();
 
 router.post("/request", handleRequest);
 router.post("/accept/:id", handleAccept);
 router.post("/remove/", handleRemove);
+
+router.get("/request", listRequests);
 
 module.exports = router;
