51b4a1e10c349bbe1b91c22ee34fa96f4b0fa005
Vijay Singh <thakurvijayofficial@gmail.com>
2024-10-25 23:40:10 +0530

    POST publish

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set("views", path.resolve("./views"));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cookieParser());
 
--- routers/user.js
+++ routers/user.js
@@ -13,4 +13,24 @@ router.post("/profile/picture", updateProfilePicture);
 router.get("/profile/picture", getProfilePicture);
 router.get("/profile", showProfile);
 
+router.post("/publish/post", (req, res) => {
+  const data = {
+    user: {
+      adm_no: req.user.adm_no,
+      name: req.user.name,
+    },
+    post: {
+      id: Math.random(3) * 10000,
+      date_uploaded: "29 August",
+      description: req.body.caption,
+    },
+    interaction: {
+      likes: 0,
+      liked: false,
+      comments: [],
+    },
+  };
+  return res.json(data);
+});
+
 module.exports = router;
